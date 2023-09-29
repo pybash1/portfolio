17f8ceb0dbb3f3b8cb49294e84252ff8361581ba
pybash1 <67195650+pybash1@users.noreply.github.com>
2023-09-29 09:05:44 +0530

    feat(404): updated 404 page and wip page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,42 +6,32 @@ interface Props {
   wip?: boolean;
 }
 
-const MSGS = [
-  "You've mistyped the URL.",
-  "Looks like you clicked the wrong link.",
-  "Trying to find secret paths?",
-  "What led you here?",
-  "Oops, you made a typo!",
-];
-
 const NotFound: NextPage<Props> = ({ wip = false }: Props) => {
   return (
     <>
       <Head>
-        <title>page not found</title>
-        <meta
-          name="description"
-          content="you prolly made a typo or clicked the wrong link. but don't worry i'll get you home."
-        />
+        <title>your lost</title>
+        <meta name="description" content={`click the link to go home.`} />
         <link
           rel="icon"
           href="https://cdn.discordapp.com/avatars/626461325744275464/9b8baa867dcbb45519996adb9397d7f4.png?size=4096"
         />
       </Head>
-      <div className="flex min-h-screen items-center justify-center bg-black font-['Labil_Grotesk'] text-lg text-white">
-        <div className="text-center">
-          {wip
-            ? "This page is still brewing. Come back later."
-            : MSGS[Math.floor(Math.random() * MSGS.length)]}
-          <br />
-          {wip ? "For now, " : null}
-          {wip ? "l" : "L"}et&apos;s take you{" "}
-          <Link href="/" className="text-green-400">
-            home
-          </Link>
-          .
+      <main
+        className={`relative flex min-h-screen flex-col items-center justify-center bg-[#faf5f1] py-8 font-['Labil_Grotesk'] leading-none text-[#ff0000] selection:bg-[#ff0000] selection:text-[#faf5f1]`}
+      >
+        <div className="text-center text-[12rem] uppercase">
+          <div className="flex items-start justify-center">
+            <div className="text-2xl lowercase text-black">click the</div>
+            <Link href="/">{wip ? "Working" : "You're"}</Link>
+            <div className="text-2xl lowercase text-black">text</div>
+          </div>
+          <Link href="/">&mdash;{wip ? "On it" : "Lost"}</Link>
         </div>
-      </div>
+        <div className="flex items-center gap-4 text-4xl">
+          {wip ? "check back in a while" : "let's go home"}
+        </div>
+      </main>
     </>
   );
 };
