00df620f9f7d83949acaaf03ea682838d1f46201
pybash1 <67195650+pybash1@users.noreply.github.com>
2023-09-30 18:15:39 +0530

    feat(projects): added all new projects page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,8 +1,88 @@
+import { AnimatePresence } from "framer-motion";
+import Link from "next/link";
 import type { NextPage } from "next";
-import NotFound from "./404";
+import Head from "next/head";
+import { useState } from "react";
+import Navbar from "~/components/Navbar";
+import Topbar from "~/components/Topbar";
 
-const WorkAtDeva: NextPage = () => {
-  return <NotFound wip={true} />;
+const Projects: NextPage = () => {
+  const [navbar, setNavbar] = useState(false);
+
+  return (
+    <>
+      <Head>
+        <title>stuff i have made</title>
+        <meta
+          name="description"
+          content={`not exactly a list of the several projects i've worked on and abandoned. you'll have to click the link to see more.`}
+        />
+        <link
+          rel="icon"
+          href="https://cdn.discordapp.com/avatars/626461325744275464/9b8baa867dcbb45519996adb9397d7f4.png?size=4096"
+        />
+      </Head>
+      <main
+        className={`relative flex min-h-screen bg-[#faf5f1] font-['Labil_Grotesk'] leading-none text-[#ff0000] selection:bg-[#ff0000] selection:text-[#faf5f1]`}
+      >
+        <AnimatePresence>{navbar ? <Navbar /> : null}</AnimatePresence>
+        <Topbar navbar={navbar} setNavbar={setNavbar} />
+        <div className="grid w-full grid-cols-4 divide-x-2 pt-24">
+          <Link
+            href="/supertable"
+            className="group flex flex-col items-center justify-between overflow-hidden px-8 text-8xl text-black transition duration-300 ease-in-out hover:text-[#ff0000]"
+          >
+            <div className="flex flex-col gap-2">
+              <div className="uppercase [overflow-wrap:anywhere]">
+                Supertable
+              </div>
+              <div className="text-xl">airtable forms but superior</div>
+            </div>
+            <div className="text-[22rem] text-[#ff0000] transition duration-300 ease-in-out group-hover:text-black">
+              1
+            </div>
+          </Link>
+          <Link
+            href="/solo"
+            className="group flex flex-col items-center justify-between overflow-hidden px-8 text-8xl text-black transition duration-300 ease-in-out hover:text-[#ff0000]"
+          >
+            <div className="flex flex-col gap-2">
+              <div className="uppercase [overflow-wrap:anywhere]">Solo</div>
+              <div className="text-xl">limited edition music marketplace</div>
+            </div>
+            <div className="text-[22rem] text-[#ff0000] transition duration-300 ease-in-out group-hover:text-black">
+              2
+            </div>
+          </Link>
+          <Link
+            href="/authdeck"
+            className="group flex flex-col items-center justify-between overflow-hidden px-8 text-8xl text-black transition duration-300 ease-in-out hover:text-[#ff0000]"
+          >
+            <div className="flex flex-col gap-2">
+              <div className="uppercase [overflow-wrap:anywhere]">Authdeck</div>
+              <div className="text-xl">
+                verify humanity without losing anonimity
+              </div>
+            </div>
+            <div className="text-[22rem] text-[#ff0000] transition duration-300 ease-in-out group-hover:text-black">
+              3
+            </div>
+          </Link>
+          <div className="group flex flex-col items-center justify-between overflow-hidden px-8 text-8xl text-black transition duration-300 ease-in-out hover:text-[#ff0000]">
+            <div className="flex flex-col gap-2">
+              <div className="uppercase [overflow-wrap:anywhere]">
+                On its Way
+              </div>
+              <div className="text-xl">another project coming soon</div>
+            </div>
+            <div className="text-[22rem] text-[#ff0000] transition duration-300 ease-in-out group-hover:text-black">
+              4
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 };
 
-export default WorkAtDeva;
+export default Projects;
