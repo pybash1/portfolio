795303b49b90d166f9120a3b690f47be6e3980e9
pybash1 <67195650+pybash1@users.noreply.github.com>
2023-10-21 15:45:47 +0530

    fix(responsiveness): made all project details responsive

--- src/pages/solo.tsx
+++ src/pages/solo.tsx
@@ -23,25 +23,25 @@ const Solo: NextPage = () => {
         />
       </Head>
       <main
-        className={`relative flex min-h-screen bg-[#faf5f1] font-['Labil_Grotesk'] leading-none text-[#ff0000] selection:bg-[#ff0000] selection:text-[#faf5f1]`}
+        className={`relative flex min-h-screen flex-col bg-[#faf5f1] font-['Labil_Grotesk'] leading-none text-[#ff0000] selection:bg-[#ff0000] selection:text-[#faf5f1] lg:flex-row`}
       >
         <AnimatePresence>{navbar ? <Navbar /> : null}</AnimatePresence>
         <Topbar navbar={navbar} setNavbar={setNavbar} />
-        <div className="fixed flex w-1/3 flex-col gap-8 px-8 pt-24 text-9xl">
+        <div className="flex w-full flex-col gap-8 px-8 pt-24 text-5xl lg:fixed lg:w-1/3 lg:text-9xl">
           <Link
             href="https://solo.pybash.xyz"
             className="[overflow-wrap:anywhere]"
           >
             Solo
           </Link>
-          <div className="flex gap-2 text-xl text-black">
+          <div className="flex gap-2 text-base text-black lg:text-xl">
             limited edition music marketplace
           </div>
-          <div className="text-center text-[20rem]">2</div>
+          <div className="hidden text-center text-[20rem] lg:block">2</div>
         </div>
-        <div className="w-1/3 border-r"></div>
-        <div className="mr-24 w-2/3 overflow-hidden border-r pt-24">
-          <div className="flex">
+        <div className="hidden w-1/3 border-r lg:block"></div>
+        <div className="overflow-hidden border-r lg:mr-24 lg:w-2/3 lg:pt-24">
+          <div className="hidden lg:flex">
             <div className="h-fit px-8 text-6xl text-black [text-orientation:mixed] [writing-mode:vertical-lr]">
               Few Details
             </div>
@@ -50,7 +50,7 @@ const Solo: NextPage = () => {
               className="w-[87%] flex-shrink object-center"
             />
           </div>
-          <div className="p-12 pl-[7.5rem] pr-10 text-xl text-gray-800">
+          <div className="p-12 pr-10 pt-12 text-xl text-gray-800 lg:pl-[7.5rem]">
             Together with @soulninja and @SpongeBoi, we built Solo as a
             decentralised marketplace for music. The platform allowed die-hard
             fans to collect songs from their favorite artists for a limited time
--- src/pages/supertable.tsx
+++ src/pages/supertable.tsx
@@ -23,25 +23,25 @@ const Supertable: NextPage = () => {
         />
       </Head>
       <main
-        className={`relative flex min-h-screen bg-[#faf5f1] font-['Labil_Grotesk'] leading-none text-[#ff0000] selection:bg-[#ff0000] selection:text-[#faf5f1]`}
+        className={`relative flex min-h-screen flex-col bg-[#faf5f1] font-['Labil_Grotesk'] leading-none text-[#ff0000] selection:bg-[#ff0000] selection:text-[#faf5f1] lg:flex-row`}
       >
         <AnimatePresence>{navbar ? <Navbar /> : null}</AnimatePresence>
         <Topbar navbar={navbar} setNavbar={setNavbar} />
-        <div className="fixed flex w-1/3 flex-col gap-8 px-8 pt-24 text-9xl">
+        <div className="flex w-full flex-col gap-8 px-8 pt-24 text-5xl lg:fixed lg:w-1/3 lg:text-9xl">
           <Link
             href="https://supertable.fun"
             className="[overflow-wrap:anywhere]"
           >
-            Supert&mdash;able
+            Supert<span className="hidden lg:block">&mdash;</span>able
           </Link>
-          <div className="flex gap-2 text-xl text-black">
+          <div className="flex gap-2 text-base text-black lg:text-xl">
             airtable but superior
           </div>
-          <div className="text-center text-[20rem]">1</div>
+          <div className="hidden text-center text-[20rem] lg:block">1</div>
         </div>
-        <div className="w-1/3 border-r"></div>
-        <div className="mr-24 w-2/3 overflow-hidden border-r pt-24">
-          <div className="flex">
+        <div className="hidden w-1/3 border-r lg:block"></div>
+        <div className="overflow-hidden border-r lg:mr-24 lg:w-2/3 lg:pt-24">
+          <div className="hidden lg:flex">
             <div className="h-fit px-8 text-6xl text-black [text-orientation:mixed] [writing-mode:vertical-lr]">
               Few Details
             </div>
@@ -50,7 +50,7 @@ const Supertable: NextPage = () => {
               className="w-[90%] flex-shrink object-center"
             />
           </div>
-          <div className="p-12 pl-[7.5rem] pr-10 text-xl text-gray-800">
+          <div className="p-12 pr-10 pt-12 text-xl text-gray-800 lg:pl-[7.5rem]">
             Supertable was a project built for{" "}
             <Link href="https://supertable.fun" className="text-[#ff0000]">
               Superteam
