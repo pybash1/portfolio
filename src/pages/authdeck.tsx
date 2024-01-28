bf9e1021af583214d6e0e463564819cd15b98dfa
pybash1 <67195650+pybash1@users.noreply.github.com>
2024-01-28 15:25:52 +0530

    refactor: deleted all unneccesary and old files

--- src/pages/authdeck.tsx
+++
@@ -1,5 +0,0 @@
-import Authdeck from "./@authdeck";
-
-const AuthdeckProject = () => <Authdeck type="project" />;
-
-export default AuthdeckProject;
--- src/pages/writing.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from "next";
-import NotFound from "./404";
-
-const WorkAtDeva: NextPage = () => {
-  return <NotFound wip={true} />;
-};
-
-export default WorkAtDeva;
